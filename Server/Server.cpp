043bc15c9bf49b29064d9bd80dc79664c905f245
ataillat <ataillat@gaming.tech>
2023-11-22 15:37:17 +0100

    webServer

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -19,10 +19,19 @@ struct ClientData {
 
 bool initializeServer(SOCKET& listeningSocket, sockaddr_in& hint, int port) {
 	
+	// Initialisation de Winsock
+	WSADATA wsData;
+	WORD ver = MAKEWORD(2, 2);
+	int wsOk = WSAStartup(ver, &wsData);
+	if (wsOk != 0) {
+		cerr << "Can't Initialize winsock! Quitting...\n";
+		return 1;
+	}
+
 	// Création du socket
 	listeningSocket = socket(AF_INET, SOCK_STREAM, 0);
 	if (listeningSocket == INVALID_SOCKET) {
-		cerr << "Can't create a socket! Quitting..." << endl;
+		cerr << "Can't create a socket! Quitting...\n";
 		return false;
 	}
 
@@ -33,21 +42,19 @@ bool initializeServer(SOCKET& listeningSocket, sockaddr_in& hint, int port) {
 
 	// Liaison du socket
 	if (bind(listeningSocket, (sockaddr*)&hint, sizeof(hint)) == SOCKET_ERROR) {
-		cerr << "Can't bind to IP/port! Quitting..." << endl;
+		cerr << "Can't bind to IP/port! Quitting...\n";
 		closesocket(listeningSocket);
-		WSACleanup();
 		return false;
 	}
 
 	// Mise en écoute du socket
 	if (listen(listeningSocket, SOMAXCONN) == SOCKET_ERROR) {
-		cerr << "Can't listen on socket! Quitting..." << endl;
+		cerr << "Can't listen on socket! Quitting...\n";
 		closesocket(listeningSocket);
-		WSACleanup();
 		return false;
 	}
 
-	cout << "Server initialized and listening on port " << port << endl;
+	cout << "Server initialized and listening on port " << port << "\n";
 	return true;
 }
 
@@ -74,18 +81,18 @@ void checkWinner(vector<zone>& zones, int& winner, SOCKET clientSocket) {
 		// Envoi d'un message au client indiquant le gagnant
 		if (winner == zone::painterList::CIRCLE) {
 			send(clientSocket, "W1", 3, 0); // Message indiquant la victoire du cercle
-			cout << "Player 1 wins!" << endl;
+			cout << "Player 1 wins!\n";
 		}
 		else if (winner == zone::painterList::CROSS) {
 			send(clientSocket, "W2", 3, 0); // Message indiquant la victoire de la croix
-			cout << "Player 2 wins!" << endl;
+			cout << "Player 2 wins!\n";
 		}
 	}
 	else {
 		// Si aucun gagnant, vous pouvez envoyer un message pour continuer le jeu ou indiquer qu'il n'y a pas encore de gagnant
 		// Exemple : 
 		send(clientSocket, "NoWinnerYet", 12, 0);
-		cout << "No winner yet!" << endl;
+		cout << "No winner yet!\n";
 	}
 }
 
@@ -99,16 +106,16 @@ void handleMove(ClientData& clientData, char* buf) {
 
 	int byteReceived = recv(clientSocket, buf, 4096, 0);
 	if (byteReceived == SOCKET_ERROR) {
-		cerr << "Error in recv(). Quitting" << endl;
+		cerr << "Error in recv(). Quitting\n";
 		return;
 	}
 
 	if (byteReceived == 0) {
-		cout << "Client is disconnected!" << endl;
+		cout << "Client is disconnected!\n";
 		return;
 	}
 
-	cout << buf << endl;
+	cout << buf << "\n";
 
 	Vector2i position;
 	if (buf[0] == 'P') {
@@ -133,7 +140,7 @@ void handleMove(ClientData& clientData, char* buf) {
 			// Envoyer un message indiquant que c'est au tour de l'autre joueur
 			string message = "Not your turn, current player: " + to_string(painter);
 			send(clientSocket, message.c_str(), message.size() + 1, 0);
-			cout << "Not your turn, current player: " << painter << endl;
+			cout << "Not your turn, current player: " << painter << "\n";
 		}
 	}
 }
@@ -146,7 +153,7 @@ DWORD WINAPI clientHandlerThread(LPVOID lpParam) {
 	int& winner = clientData->winner;
 
 	// Message pour indiquer qu'un client s'est connecté
-	cout << "Client connected!" << endl;
+	cout << "Client connected!\n";
 
 	char buf[4096];
 	while (true) {
@@ -154,17 +161,17 @@ DWORD WINAPI clientHandlerThread(LPVOID lpParam) {
 
 		int byteReceived = recv(clientSocket, buf, sizeof(buf), 0);
 		if (byteReceived == SOCKET_ERROR) {
-			cerr << "Error in recv(). Quitting" << endl;
+			cerr << "Error in recv(). Quitting\n";
 			break;
 		}
 
 		if (byteReceived == 0) {
-			cout << "Client is disconnected!" << endl;
+			cout << "Client is disconnected!\n";
 			break;
 		}
 
 		// Traitement des données reçues du client
-		cout << "Received from client: " << buf << endl;
+		cout << "Received from client: " << buf << "\n";
 
 		// Appeler la fonction pour gérer les mouvements du joueur
 		handleMove(*clientData, buf);
@@ -178,7 +185,7 @@ DWORD WINAPI clientHandlerThread(LPVOID lpParam) {
 
 // Fonction pour le serveur principal
 DWORD WINAPI serverMain(LPVOID lpParam) {
-	cout << "Server Main thread running...\n" << endl;
+	cout << "Server Main thread running...\n";
 	
 	// Déclaration des variables pour le serveur
 	SOCKET listening;
@@ -205,12 +212,12 @@ DWORD WINAPI serverMain(LPVOID lpParam) {
 			// Ici, vous pouvez gérer la logique pour les spectateurs et les joueurs dans le même thread
 			// Par exemple, examinez si un joueur est déjà connecté, puis assignez le nouveau client comme spectateur
 			if (clients.size() <= 2) {
-				cout << "Player connected!" << endl; // Message lorsque qu'un joueur se connecte
+				cout << "Player connected!\n"; // Message lorsque qu'un joueur se connecte
 				// Traitez les joueurs
 				// Assignez le joueur et initiez le jeu si nécessaire
 			}
 			else {
-				cout << "Spectator connected!" << endl; // Message lorsque qu'un joueur se connecte
+				cout << "Spectator connected!\n"; // Message lorsque qu'un joueur se connecte
 				// Traitez les spectateurs
 				// Affichez l'état actuel du jeu ou d'autres informations pertinentes aux spectateurs
 			}
@@ -226,35 +233,58 @@ DWORD WINAPI serverMain(LPVOID lpParam) {
 
 // Fonction pour le serveur web
 DWORD WINAPI webServer(LPVOID lpParam) {
-	cout << "Web Server thread running...\n" << endl;
-	// Mettez ici le code du serveur web
-	// Il peut gérer les requêtes HTTP pour afficher l'état du jeu sur un navigateur
+	cout << "Web Server thread running...\n";
+
+	SOCKET webSocket;
+	sockaddr_in webHint;
+	char buf[4096];
+
+	// Initialisation du serveur web
+	if (!initializeServer(webSocket, webHint, 5005)) {
+		return 0; // Quitter le thread en cas d'échec de l'initialisation
+	}
+
+	// Message à afficher dans la fenêtre web
+	string message = "<html><body><h1>Bienvenue sur le serveur de jeu</h1></body></html>";
+
+	// Boucle de gestion des requêtes web
+	while (true) {
+		SOCKET clientWebSocket = accept(webSocket, nullptr, nullptr);
+		if (clientWebSocket != INVALID_SOCKET) {
+			cout << "Web client connected!\n";
+
+			// Répondre à la requête avec un message HTML
+			string response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+			response += message;
+
+			send(clientWebSocket, response.c_str(), response.size(), 0);
+
+			// Fermer la connexion avec le client web
+			closesocket(clientWebSocket);
+		}
+	}
+
+	// Fermeture du serveur web
+	closesocket(webSocket);
+	WSACleanup();
+
 	return 0;
 }
 
 int main() {
-	// Initialisation de Winsock
-	WSADATA wsData;
-	WORD ver = MAKEWORD(2, 2);
-	int wsOk = WSAStartup(ver, &wsData);
-	if (wsOk != 0) {
-		cerr << "Can't Initialize winsock! Quitting..." << endl;
-		return 1;
-	}
-
 	HANDLE serverThread = CreateThread(NULL, 0, serverMain, NULL, CREATE_NO_WINDOW, NULL);
 	if (serverThread == NULL) {
-		cerr << "Failed to create server thread!" << endl;
+		cerr << "Failed to create server thread!\n";
 		return 1;
 	}
 
 	HANDLE webThread = CreateThread(NULL, 0, webServer, NULL, CREATE_NO_WINDOW, NULL);
 	if (webThread == NULL) {
-		cerr << "Failed to create web server thread!" << endl;
+		cerr << "Failed to create web server thread!\n";
 		return 1;
 	}
 
-	cout << "Main thread running...\n" << endl;
+	cout << "Main thread running...\n";
 	// Logique du thread principal (interface utilisateur, traitement supplémentaire, etc.)
 	// ... 
 
@@ -264,8 +294,6 @@ int main() {
 
 	CloseHandle(serverThread);
 	CloseHandle(webThread);
-
-	WSACleanup();
 
 	return 0;
 }
